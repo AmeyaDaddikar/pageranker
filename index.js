8aef0974a49a4deb58e85128bae212ebde3697ed
Ameya Daddikar <ameyadaddikar@gmail.com>
2020-02-19 13:00:54 +0530

    cleaned code and add new code snippet

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const url = require('url');
-const LinkCrawler = require('./src/linkcrawler');
 const PageRanker  = require('./src/pageranker');
 
-new PageRanker(
-  'vjti.ac.in',  // host on which pagerank is applied
-   false,        // allow cross domain (false means only pages of the given host will be crawled)
-   2,            // MAX Iterations of pagerank
-   0.4           // Damping Factor
-   ).rank('', 'site_map_x_vjti.ac.in.json', (vals = []) => {
+function rankerCalback(vals = []) {
   let sum = 0;
   vals.forEach(l => sum += l.PR);
   console.log(`Sum of PRs:: ${sum}`);
 
   const rankedList = vals.sort((a, b) => b.PR - a.PR);
   console.log('MAX PR:: ', rankedList[0])
-});
+} 
+
+const pageranker = new PageRanker(
+        'vjti.ac.in',  // host on which pagerank is applied
+        false,        // allow cross domain (false means only pages of the given host will be crawled)
+        2,            // MAX Iterations of pagerank
+        0.4           // Damping Factor
+);
+
+pageranker.rank(
+  '',                          // start route
+  'site_map_vjti.ac.in.json',  // file to search for cached pagerank object
+  rankerCalback                // callback
+);
